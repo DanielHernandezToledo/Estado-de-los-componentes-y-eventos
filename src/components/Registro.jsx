efa55c3071f177e9b60439fc0a50a2772a41e4c6
luisfersan <lfersan@gmail.com>
2024-05-27 11:53:04 -0500

    feat/Se agrega el import para el componente SocialButton en el componente Registro.jsx

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -1,13 +1,17 @@
+// Importando los componentes al componente Registro.jsx
+
 import SocialButton from "./SocialButton";
 
-export default function Registro(){
-
-    return(
-        <div className="card" style={{width:'24rem'}}>
-            <h1 className="text-primary text-center">Crear una cuenta</h1>
-            <div className="d-flex justify-content-evenly gap-2">
-                <SocialButton />
-            </div>
-        </div>
-    )
+export default function Registro() {
+  return (
+    <div
+      className="card"
+      style={{ width: "24rem" }}
+    >
+      <h1 className="text-primary text-center">Crear una cuenta</h1>
+      <div className="d-flex justify-content-evenly gap-2">
+        <SocialButton />
+      </div>
+    </div>
+  );
 }
