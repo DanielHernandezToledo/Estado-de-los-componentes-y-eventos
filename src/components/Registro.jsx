99ad0681d306aea238a0233444ec84391be91757
DanielHernandezToledo <daniel.aht1993@gmail.com>
2024-05-27 11:52:58 -0400

    Feat Se agrega bootstrap, 4 componentes e iconos

---
+++ src/components/Registro.jsx
@@ -0,0 +1,13 @@
+import SocialButton from "./SocialButton";
+
+export default function Registro(){
+
+    return(
+        <div className="card" style={{width:'24rem'}}>
+            <h1 className="text-primary text-center">Crear una cuenta</h1>
+            <div className="d-flex justify-content-evenly gap-2">
+                <SocialButton />
+            </div>
+        </div>
+    )
+}
---
+++ src/components/SocialButton.jsx
@@ -0,0 +1,16 @@
+import facebook from '../../public/icons/facebook.png'
+import github from '../../public/icons/github.png'
+import linkedin from '../../public/icons/linkedin.png'
+
+export default function SocialButton(){
+
+    const tamaño = {width: '40px'}
+
+    return(
+        <>
+            <a href="www.facebook.com"><img src={facebook} alt="Facebook" style={tamaño} className='border'/></a>
+            <a href="www.github.com"><img src={github} alt="Github" style={tamaño} className='border'/></a>
+            <a href="www.linkedin.com"><img src={linkedin} alt="Linkedin" style={tamaño} className='border'/></a>
+        </>
+    )
+}
